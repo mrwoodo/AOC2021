0496fc09bfb2ca3db1b32e634f1fdb8a314d7d1f
Chris Woodworth <chris.woodworth@icloud.com>
2021-12-03 11:06:42 +1100

    Day 2

--- AOC2021/Day02.cs
+++ AOC2021/Day02.cs
@@ -12,12 +12,51 @@ namespace AOC2021
 
         public string Part1()
         {
-            return "";
+            var movements = new Dictionary<string, int>();
+
+            movements["down"] = 0;
+            movements["up"] = 0;
+            movements["forward"] = 0;
+
+            foreach (var s in this.InputFileAsStringList)
+            {
+                var command = s.Split(' ');
+                var direction = command[0];
+                var amount = int.Parse(command[1]);
+
+                movements[direction] += amount;
+            }
+
+            var horizontal = movements["forward"];
+            var vertical = movements["down"] - movements["up"];
+
+            return $"{horizontal * vertical}";
         }
 
         public string Part2()
         {
-            return "";
+            int aim = 0;
+            var horizontal = 0;
+            var vertical = 0;
+
+            foreach (var s in this.InputFileAsStringList)
+            {
+                var command = s.Split(' ');
+                var direction = command[0];
+                var amount = int.Parse(command[1]);
+
+                if (direction == "down")
+                    aim += amount;
+                else if (direction == "up")
+                    aim -= amount;
+                else
+                {
+                    horizontal += amount;
+                    vertical += aim * amount;
+                }
+            }
+
+            return $"{horizontal * vertical}";
         }
     }
 }
