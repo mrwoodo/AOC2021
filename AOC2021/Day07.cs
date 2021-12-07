7604b194ae592d3e23e4f0384cbb6675f3206538
Chris Woodworth <chris.woodworth@icloud.com>
2021-12-07 22:53:54 +1100

    Precomputing fuel steps - Remove hardcoded min/max

--- AOC2021/Day07.cs
+++ AOC2021/Day07.cs
@@ -7,7 +7,7 @@ namespace AOC2021
     public class Day07 : DayBase, ITwoPartQuestion
     {
         public List<int> Crabs = new List<int>();
-        private readonly Dictionary<int, int> Factors = new Dictionary<int, int>();
+        private readonly Dictionary<int, int> Steps = new Dictionary<int, int>();
 
         public Day07()
         {
@@ -19,7 +19,7 @@ namespace AOC2021
 
         public string Part1()
         {
-            var BestFuelUsage = int.MaxValue;
+            var BestFuelCost = int.MaxValue;
 
             for (int i = 0; i < Crabs.Count; i++)
             {
@@ -28,41 +28,41 @@ namespace AOC2021
                 for (int j = 0; j < Crabs.Count; j++)
                     FuelCost += Math.Abs(Crabs[j] - Crabs[i]);
 
-                if (FuelCost < BestFuelUsage)
-                    BestFuelUsage = FuelCost;
+                if (FuelCost < BestFuelCost)
+                    BestFuelCost = FuelCost;
             }
 
-            return $"{BestFuelUsage}";
+            return $"{BestFuelCost}";
         }
 
         public string Part2()
         {
-            var BestFuelUsage = int.MaxValue;
+            PreComputeFuelSteps();
 
-            CalculateFactors();
+            var BestFuelCost = int.MaxValue;
 
             for (int i = Crabs.Min(); i <= Crabs.Max(); i++)
             {
                 var FuelCost = 0;
 
                 for (int j = 0; j < Crabs.Count; j++)
-                    FuelCost += Factors[Math.Abs(Crabs[j] - i)];
+                    FuelCost += Steps[Math.Abs(Crabs[j] - i)];
 
-                if (FuelCost < BestFuelUsage)
-                    BestFuelUsage = FuelCost;
+                if (FuelCost < BestFuelCost)
+                    BestFuelCost = FuelCost;
             }
 
-            return $"{BestFuelUsage}";
+            return $"{BestFuelCost}";
         }
 
-        private void CalculateFactors()
+        private void PreComputeFuelSteps()
         {
             var counter = 0;
 
-            for (int i = 0; i < 2000; i++)
+            for (int i = Crabs.Min(); i <= Crabs.Max(); i++)
             {
                 counter += i;
-                Factors[i] = counter;
+                Steps[i] = counter;
             }
         }
     }
