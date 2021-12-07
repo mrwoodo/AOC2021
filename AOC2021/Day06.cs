daa612960bff1a61d72a3c038c7d6a910b4a62e8
Chris Woodworth <chris.woodworth@icloud.com>
2021-12-07 15:10:03 +1100

    Day 06

--- AOC2021/Day06.cs
+++ AOC2021/Day06.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AOC2021
 {
@@ -12,12 +13,63 @@ namespace AOC2021
 
         public string Part1()
         {
-            return "";
+            var fishies = this.InputFile.Replace(",", "");
+            var rules = new Dictionary<char, char>
+            {
+                ['8'] = '7', ['7'] = '6', ['6'] = '5', ['5'] = '4',
+                ['4'] = '3', ['3'] = '2', ['2'] = '1', ['1'] = '0',
+                ['0'] = '6'
+            };
+
+            for (int days = 0; days < 80; days++)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var fish in fishies)
+                {
+                    sb.Append(rules[fish]);
+                    if (fish == '0')
+                        sb.Append('8');
+                }
+
+                fishies = sb.ToString();
+            }
+
+            return $"{fishies.Length}";
         }
 
         public string Part2()
         {
-            return "";
+            var fishTally = new Dictionary<int, long>
+            {
+                [0] = 0, [1] = 0, [2] = 0, [3] = 0,
+                [4] = 0, [5] = 0, [6] = 0, [7] = 0,
+                [8] = 0
+            };
+
+            foreach (var fish in this.InputFile.Split(','))
+                fishTally[int.Parse(fish)]++;
+
+            for (int days = 0; days < 256; days++)
+            {
+                var temp = fishTally[0];
+
+                fishTally[0] = fishTally[1];
+                fishTally[1] = fishTally[2];
+                fishTally[2] = fishTally[3];
+                fishTally[3] = fishTally[4];
+                fishTally[4] = fishTally[5];
+                fishTally[5] = fishTally[6];
+                fishTally[6] = fishTally[7] + temp; //including rollover from 0 to 6
+                fishTally[7] = fishTally[8];
+                fishTally[8] = temp; //new fish
+            }
+
+            long answer = 0;
+            for (int i = 0; i < 9; i++)
+                answer += fishTally[i];
+
+            return $"{answer}";
         }
     }
 }
